e786cb30ae6f9feb1956f18e939776096a7a85f5
Tsmith052 <35273895+Tsmith05252@users.noreply.github.com>
2023-09-05 18:03:04 -0500

    Added two files
    created template for two pages

---
+++ api/frontend/src/pages/ChangeThermostatLog.jsx
@@ -0,0 +1,13 @@
+/* Page where user can change the log and time    */ 
+function ChangeThermostatLog() 
+{
+    return(
+
+        <div>
+
+        </div>
+
+    ); 
+}
+
+export default ChangeThermostatLog(); 
