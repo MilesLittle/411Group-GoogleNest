c3ec4be815480e00b95d618b6245efaec08f53eb
NRanel <99266490+NRanel@users.noreply.github.com>
2023-09-06 20:04:12 -0500

    routes, pages

--- api/frontend/src/index.js
+++ api/frontend/src/index.js
@@ -6,15 +6,30 @@ import './index.css';
 import App from './App'
 import './css/index.css'
 import AuthProvider from './Login/AuthProvider';
+import { createBrowserRouter, createRoutesFromElements, Route, RouterProvider} from "react-router-dom";
+import Home from './Pages/Home';
+import Login from './Pages/Login';
+import NotFound from './Pages/NotFound';
 
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <>
+      <Route path="/login" element={<Login/>}/>
+      <Route path="/" element={<Home />}/>
+      <Route path="*" element={<NotFound />}/>
+    </>
+  )
+);
 // @ts-ignore
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
 <GoogleOAuthProvider clientId='589825515650-ej6sq8icgc3itevo7b731oes8q1tqk4u.apps.googleusercontent.com'>
   <React.StrictMode>
-    <AuthProvider>
-      <App />
-    </AuthProvider>
+    <RouterProvider router={router}>
+      <AuthProvider>
+        <App />
+      </AuthProvider>
+    </RouterProvider>
   </React.StrictMode>
 </GoogleOAuthProvider>
 );
