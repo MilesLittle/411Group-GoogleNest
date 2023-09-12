7799ef99445b71024ef1a7abd4a380b0f109c62f
NRanel <99266490+NRanel@users.noreply.github.com>
2023-09-11 22:22:43 -0500

    navbar progress

--- api/frontend/src/components/NavBar/NavBar.js
+++ api/frontend/src/components/NavBar/NavBar.js
@@ -6,23 +6,37 @@ import Typography from '@mui/material/Typography';
 import Button from '@mui/material/Button';
 import Grid from '@mui/material/Grid';
 import DarkModeSwitch from './DarkModeSwitch';
+import Card from '@mui/material/Card';
+import Avatar from '@mui/material/Avatar';
 
 const NavBar = () => {
   return (
     <Box sx={{ flexGrow: 1, marginTop: '1rem', marginBottom: '1rem', marginLeft: '4rem', marginRight: '4rem', position: 'sticky', top: '0' }}>
-      <AppBar position="sticky" sx={{ borderRadius: '30px' }}> 
+      <AppBar position="sticky" sx={{ borderRadius: '30px' }} elevation={10}> 
         <Toolbar>
         <Grid container direction="row" justifyContent="space-between" alignItems="center">
           <Grid item>
             <DarkModeSwitch />
           </Grid>
           <Grid item>
-            <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
-              TempWise
+            <Typography variant="h6">
+              We don't have a logo yet
             </Typography>
           </Grid>
           <Grid item>
-            <Button color="inherit">Login</Button>
+            <Card sx={{ padding: '3px', borderRadius: '30px', backgroundColor: 'secondary.main', width: '13rem'}}>
+              <Grid container direction="row" alignItems="center">
+                <Grid item>
+                  <Avatar 
+                    alt="profile pic" 
+                    src='https://wickedhorror.com/wp-content/uploads/2022/08/terminator-teeth-864x467.jpg' 
+                  />
+                </Grid>
+                <Grid item>
+                  <Typography sx={{ color: 'secondary.light' }}>Terminator</Typography>
+                </Grid>
+              </Grid>
+            </Card>
           </Grid>
         </Grid>
         </Toolbar>
