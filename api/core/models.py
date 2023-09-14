c34db81f99c553d34c2106912f95abbc8a8730cf
SirSwooth <brady.landry@selu.edu>
2023-09-13 20:41:09 -0500

    TempInfo Model Change
    Added time_stamp field to TempInfo, is set to current time when new record is created

--- api/core/models.py
+++ api/core/models.py
@@ -1,11 +1,15 @@
 from django.db import models
+import datetime
+
 
 # Create your models here.
 
 #This is test info i put in to test the app
 class TempInfo(models.Model):
+
     temp = models.CharField(max_length = 4)
     humidity = models.CharField(max_length = 4)
+    time_stamp = models.DateTimeField(auto_now = True)
 
     def __str__(self):
         return self.temp
