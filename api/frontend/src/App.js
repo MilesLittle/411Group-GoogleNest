ed6fff143eda1851fc5cdd8ad134c3b60e27fbb4
NRanel <99266490+NRanel@users.noreply.github.com>
2023-09-14 16:13:58 -0500

    Thermostat card, dashboard components

--- api/frontend/src/App.js
+++ api/frontend/src/App.js
@@ -3,6 +3,7 @@ import Home from "./Pages/Home";
 import NotFound from "./Pages/NotFound";
 import NavBar from "./components/NavBar/NavBar";
 import Profile from "./Pages/Profile";
+import ThermoDashboard from "./Pages/ThermoDashboard";
 import { Routes, Route } from "react-router-dom";
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
       <NavBar />
       <Routes>
         <Route path="/profile" element={<Profile />}/>
+        <Route path="/thermo/:id" element={<ThermoDashboard />}/> {/*protect this route later */}
         <Route path="/" element={<Home />}/>
         <Route path="*" element={<NotFound />}/>
       </Routes>
--- api/frontend/src/components/ThermoCard/ThermoCard.js
+++ api/frontend/src/components/ThermoCard/ThermoCard.js
@@ -5,8 +5,10 @@ import CardMedia from '@mui/material/CardMedia';
 import Grid from '@mui/material/Grid';
 import Typography from '@mui/material/Typography'
 import TuneIcon from '@mui/icons-material/Tune';
+import { useNavigate } from "react-router-dom";
 
 const ThermoCard = ({ id, deviceName, type, location }) => { //later, pass in some thermostat details from Google API as props instead of static data
+    const navigate = useNavigate()
     return (
         <Card sx={{ maxWidth: 400, borderRadius: '2rem', bgcolor: 'primary.main' }} elevation={15} key={id}>
             <CardMedia sx={{ height: 300, borderRadius: '2rem' }} image={'https://consumersenergystore.com/dw/image/v2/BDDP_PRD/on/demandware.static/-/Sites-masterCatalog/default/dw435871c9/Products/I-NSTTSTATX-01-BLCK-XXXX-V1.jpg?sw=800&sh=800'} title='Nest Device' />
@@ -18,7 +20,9 @@ const ThermoCard = ({ id, deviceName, type, location }) => { //later, pass in so
                         </Typography>
                     </Grid>
                     <Grid item>
-                        <TuneIcon />
+                        <div onClick={() => navigate(`/thermo/${id}`)} style={{ cursor: 'pointer'}}>
+                            <TuneIcon />
+                        </div>
                     </Grid>
                 </Grid>
                 <Grid container direction="column" justifyContent="flex-start">
