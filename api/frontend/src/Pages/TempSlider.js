9338f420ce4767782b9410c7d1fb096c85963220
Tsmith052 <35273895+Tsmith05252@users.noreply.github.com>
2023-10-14 16:31:37 -0500

    added tempature slider page

---
+++ api/frontend/src/Pages/TempSlider.js
@@ -0,0 +1,24 @@
+import * as React from 'react'; 
+import Switch from '@mui/material/Switch';
+
+
+const TempSlider = () => {
+
+return (
+    <>
+        <div style={{textAlign: 'center'}} >
+            <h1> DeviceName 1 </h1>
+            
+            
+
+            
+        </div>
+
+    
+    </>
+
+
+)} 
+
+
+export default TempSlider
