0734f512bdad7cbb7ab0b55fb514dfafc6a58b26
Tsmith052 <35273895+Tsmith05252@users.noreply.github.com>
2023-10-14 14:38:14 -0500

    added modal functionality

--- api/frontend/src/Pages/ChangeLog.js
+++ api/frontend/src/Pages/ChangeLog.js
@@ -1,26 +1,51 @@
 import * as React from 'react'; 
-//import { styled, alpha } from '@mui/system';
-//import { Box } from '@mui/material';
-//import Grid from '@mui/material/Grid';
-//import Button from '@mui/material/Button';
-//import Menu from '@mui/material/Menu';
-//import MenuItem from '@mui/material/MenuItem';
-//import KeyboardArrowDownIcon from '@mui/icons-material/KeyboardArrowDown';
-import Typography from '@mui/material/Typography';
-//import TextField from '@mui/material/TextField';
+import Button from '@mui/material/Button';
+import { useState } from 'react';
+import Dialog from '@mui/material/Dialog'
+import DialogActions from '@mui/material/DialogActions';
+import DialogContent from '@mui/material/DialogContent';
+import DialogContentText from '@mui/material/DialogContentText';
+import DialogTitle from '@mui/material/DialogTitle';
 
 
 const ChangeLog = () =>  {  
+
+// Open and close the button for the modal 
+const [open, setOpen] = React.useState(false);
+
+const OpenModal = () => {
+  setOpen(true); 
+}
+
+const CloseModal = () => {
+  setOpen(false); 
+}
+
+// get the number and time -> days minutes hours 
+
 // React Stuff 
     return (
-      <>
-        
-      
-      
-      </>
-        
-    )
+    <>
+      <div style={{textAlign: 'center'}}>
+          <Button onClick={OpenModal} color="primary" variant="contained"> Test Button </Button>
 
+          <Dialog open={open} onClose={CloseModal}>
+            <DialogTitle> Thermostat 1 Log Setting </DialogTitle> 
+              <DialogContent>  
+                <DialogContentText> Test </DialogContentText>
+
+
+
+                </DialogContent>
+
+            <DialogActions> 
+              <Button onClick={CloseModal} color="secondary"> Cancel </Button>
+              <Button onClick={CloseModal} color="primary"> Save </Button>
+            </DialogActions>
+          </Dialog>
+        </div>
+    </>     
+  ); 
 }
 
 export default ChangeLog 
